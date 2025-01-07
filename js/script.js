a49fdec5feb274f6d82565ea56fc48f4bb962c74
Angel Olivares <aaommago@proton.me>
2025-01-06 21:48:31 -0300

    cambio de forEach por bucles for (revisar comentarios en js/script.js)

--- js/script.js
+++ js/script.js
@@ -1,23 +1,26 @@
 let botonGenero = document.querySelectorAll(".menu ul li");
 let titulo = document.querySelector(".libros h2");
 
-botonGenero.forEach(button => {
-    button.addEventListener("click", () => {
-        titulo.textContent = button.getAttribute("data-genre");
-    });
-});
+//de acuerdo a lo que hacia "forEach" que era recorrer los elementos, el raciocinio a aplicar fue, ¿De que otra manera, que se haya visto en clases, puedo recorrer mis elementos? y se me ocurrio el uso de bucles, lo que a nivel de entendimiento se hace mucho mas sencillo, en este caso el bucle "for" fue el que escogi para no tener que declarar mas variables "por fuera" y que queden dentro de la declaracion del bucle. Entonces cual es la idea al usar un bucle For, recorrer los elementos de botonGenero usando un indice "i" y al mismo tiempo este indice es asignado como "identificador" para cada elemento y de acuerdo a lo que se declara dentro de la funcion flecha, cuando a cualquiera de estos elemento se le haga un click se vera modificado el texto "Todos los libros" ubicado en ".libros h2", por ejemplo si se hace un click en "Autoayuda" este ya esta identificado como botonGenero[3], entonces titulo.innerText cambia por botonGenero[3].innerText
 
+for (let i = 0; i < botonGenero.length; i++) {
+    botonGenero[i].addEventListener("click", () => {
+        titulo.innerText = botonGenero[i].innerText;
+    });
+}
 
 let botonRentar = document.querySelectorAll(".libro button");
 
-botonRentar.forEach(button => {
-    button.addEventListener("click", () => {
-        button.textContent = "No Disponible";
-        button.style.backgroundColor = "#990000";
-        button.style.color = "#fff675";
-        button.disabled = true;
+//La logica que se aplico en este caso obedece a la misma explicada anteriormente, cambiando el uso de un "forEach" por un bucle "for"
+
+for (let i = 0; i < botonRentar.length; i++) {
+    botonRentar[i].addEventListener("click", () => {
+        botonRentar[i].innerText = "No Disponible";
+        botonRentar[i].style.backgroundColor = "#990000";
+        botonRentar[i].style.color = "#fff675";
+        botonRentar[i].disabled = true;
     });
-});
+}
 
 document.getElementById("login").addEventListener("click", () => {
     alert("Inicio de sesión exitoso");
